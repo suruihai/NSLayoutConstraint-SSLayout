9df71700b0b1127f5bf13cf5983af66fedb7ab4e
suruihai <suruihai@sogou-inc.com>
2017-01-24 15:28:17 +0800

    init almost done

--- SSLayoutEfficiencyTest/SSLayoutEfficiencyTest/ViewController.m
+++ SSLayoutEfficiencyTest/SSLayoutEfficiencyTest/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "NSLayoutConstraint+SSLayout.h"
+#import <Masonry.h>
 #import <objc/runtime.h>
 
 @interface ViewController ()
@@ -27,8 +28,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.automaticallyAdjustsScrollViewInsets = YES;
-    [self setupUI];
 }
 
 - (void)dealloc {
@@ -36,6 +35,10 @@
 }
 
 - (void)setupUI {
+    [self.blueView removeFromSuperview];
+    [self.redView removeFromSuperview];
+    self.blueView = nil;
+    self.redView = nil;
     [self.view addSubview:self.blueView];
     [self.view addSubview:self.redView];
 }
@@ -65,19 +68,26 @@
 }
 
 - (void)clear {
-    [self.view removeConstraints:self.view.constraints];
-    [self.blueView removeConstraints:self.blueView.constraints];
-    objc_removeAssociatedObjects(self.blueView);
+    
+    [self setupUI];
+    
+    [self.redView activateConstraints:^{
+        self.redView.height_attr.constant = 100;
+        self.redView.width_attr.constant = 50;
+        [self.redView.top_attr equalTo:self.view.top_attr constant:300];
+        [self.redView.leading_attr equalTo:self.view.leading_attr constant:200];
+    }];
 }
 
+// SSLayout Abbreviated Usage
 - (IBAction)SSAB {
     [self clear];
     double start = CFAbsoluteTimeGetCurrent();
-    for (int i=0; i<10000; i++) {
+    for (int i=0; i<1000; i++) {
         [self.blueView activateConstraints:^() {
             self.blueView.height_attr.constant = 50;
-            self.blueView.width_attr.constant = 50;
-            self.blueView.top_attr = self.view.top_attr;
+            self.blueView.width_attr = self.redView.width_attr;
+            self.blueView.top_attr = self.redView.bottom_attr;
             self.blueView.leading_attr = self.view.leading_attr;
         }];
     }
@@ -86,14 +96,15 @@
     //    NSLog(@"blueview constraints---%@", self.blueView.constraints);
 }
 
+// SSLayout Normal Usage
 - (IBAction)SSNM {
     [self clear];
     double start = CFAbsoluteTimeGetCurrent();
-    for (int i=0; i<10000; i++) {
+    for (int i=0; i<1000; i++) {
         [self.blueView activateConstraints:^() {
             [self.blueView.height_attr equalTo:nil constant:50];
-            [self.blueView.width_attr equalTo:nil constant:50];
-            [self.blueView.top_attr equalTo:self.view.top_attr];
+            [self.blueView.width_attr equalTo:self.redView.width_attr];
+            [self.blueView.top_attr equalTo:self.redView.bottom_attr];
             [self.blueView.leading_attr equalTo:self.view.leading_attr];
         }];
     }
@@ -102,16 +113,17 @@
     //    NSLog(@"blueview constraints---%@", self.blueView.constraints);
 }
 
+// NSLayoutAnchor
 - (IBAction)ANCHOR {
     [self clear];
     if (self.blueView.translatesAutoresizingMaskIntoConstraints) {
         self.blueView.translatesAutoresizingMaskIntoConstraints = NO;
     }
     double start = CFAbsoluteTimeGetCurrent();
-    for (int i=0; i<10000; i++) {
+    for (int i=0; i<1000; i++) {
         [self.view addConstraint:[self.blueView.heightAnchor constraintEqualToConstant:50]];
-        [self.view addConstraint:[self.blueView.widthAnchor constraintEqualToConstant:50]];
-        [self.view addConstraint:[self.blueView.topAnchor constraintEqualToAnchor:self.view.topAnchor]];
+        [self.view addConstraint:[self.blueView.widthAnchor constraintEqualToAnchor:self.redView.widthAnchor]];
+        [self.view addConstraint:[self.blueView.topAnchor constraintEqualToAnchor:self.redView.bottomAnchor]];
         [self.view addConstraint:[self.blueView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor]];
     }
     NSLog(@"duration---%f", CFAbsoluteTimeGetCurrent() - start);
@@ -119,16 +131,17 @@
     //    NSLog(@"blueview constraints---%@", self.blueView.constraints);
 }
 
+// System Default Method appointed constraint owner
 - (IBAction)DEFAULT {
     [self clear];
     if (self.blueView.translatesAutoresizingMaskIntoConstraints) {
         self.blueView.translatesAutoresizingMaskIntoConstraints = NO;
     }
     double start = CFAbsoluteTimeGetCurrent();
-    for (int i=0; i<10000; i++) {
+    for (int i=0; i<1000; i++) {
         [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:50 ss_priority:UILayoutPriorityRequired]];
-        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:50 ss_priority:UILayoutPriorityRequired]];
-        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
+        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.redView attribute:NSLayoutAttributeWidth multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
+        [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.redView attribute:NSLayoutAttributeBottom multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
         [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
     }
     NSLog(@"duration---%f", CFAbsoluteTimeGetCurrent() - start);
@@ -136,17 +149,18 @@
     //    NSLog(@"blueview constraints---%@", self.blueView.constraints);
 }
 
+// System Default Method without appointed owner
 - (IBAction)DEFAULT2 {
     [self clear];
     if (self.blueView.translatesAutoresizingMaskIntoConstraints) {
         self.blueView.translatesAutoresizingMaskIntoConstraints = NO;
     }
     double start = CFAbsoluteTimeGetCurrent();
-    for (int i=0; i<10000; i++) {
+    for (int i=0; i<1000; i++) {
         NSMutableArray *arrayM = [NSMutableArray arrayWithCapacity:4];
         [arrayM addObject:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:50 ss_priority:UILayoutPriorityRequired]];
-        [arrayM addObject:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:50 ss_priority:UILayoutPriorityRequired]];
-        [arrayM addObject:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
+        [arrayM addObject:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.redView attribute:NSLayoutAttributeWidth multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
+        [arrayM addObject:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.redView attribute:NSLayoutAttributeBottom multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
         [arrayM addObject:[NSLayoutConstraint constraintWithItem:self.blueView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1 constant:0 ss_priority:UILayoutPriorityRequired]];
         [NSLayoutConstraint activateConstraints:arrayM];
     }
@@ -158,11 +172,11 @@
 - (IBAction)MASONRY {
     [self clear];
     double start = CFAbsoluteTimeGetCurrent();
-    for (int i=0; i<10000; i++) {
+    for (int i=0; i<1000; i++) {
         [self.blueView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.height.mas_equalTo(50);
-            make.width.mas_equalTo(50);
-            make.top.equalTo(self.view.mas_top);
+            make.width.equalTo(self.redView.mas_width);
+            make.top.equalTo(self.redView.mas_bottom);
             make.leading.equalTo(self.view.mas_leading);
         }];
     }
